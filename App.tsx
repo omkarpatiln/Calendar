06bdc4abebde9e6fade8ed825d127578e44bffe1
omkarpatiln <97469345+omkarpatiln@users.noreply.github.com>
2025-07-02 18:11:35 +0530

    git
    issues solved

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import Calender from './src/Screens/Calender/Calender';
+
+const App = () => {
+  return <Calender />;
+};
+
+export default App;
